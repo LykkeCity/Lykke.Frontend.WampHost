bd84d19fb41da097b9c0f5dd7e2fecc2a24e6379
Mikhail Musin <mikhail.musin@gmail.com>
2017-09-20 00:48:20 +0300

    LWDEV-2869 documentation + bugfix

--- src/Lykke.Frontend.WampHost.Services/Documentation/IWampTopic.cs
+++ src/Lykke.Frontend.WampHost.Services/Documentation/IWampTopic.cs
@@ -4,7 +4,7 @@ namespace Lykke.Frontend.WampHost.Services.Documentation
 {
     public interface IWampTopics
     {
-        [DocMe(Name = "candle.{spot|mt}.{instrument}.{bid|ask|mid}.{sec|minute|min5|min15|min30|hour|hour4|hour6|hour12|day|week|month}", Description = "sends candles")]
+        [DocMe(Name = "candle.{spot|mt}.{instrument}.{bid|ask|mid}.{sec|minute|min5|min15|min30|hour|hour4|hour6|hour12|day|week|month}", Description = "sends candles. realm = 'prices', all parameters in the lower case.")]
         CandleMessage CandlesUpdate();        
     }
 }
