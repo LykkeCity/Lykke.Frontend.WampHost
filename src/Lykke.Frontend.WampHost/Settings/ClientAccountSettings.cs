408f40a5aa3eeca3a6ad7b490a7d42a4ba6309df
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-01-09 21:31:03 +0300

    updated packages

--- src/Lykke.Frontend.WampHost/Settings/ClientAccountSettings.cs
+++ src/Lykke.Frontend.WampHost/Settings/ClientAccountSettings.cs
@@ -1,7 +1,10 @@
-﻿namespace Lykke.Frontend.WampHost.Settings
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Frontend.WampHost.Settings
 {
     public class ClientAccountSettings
     {
+        [HttpCheck("/api/isalive")]
         public string ServiceUrl { get; set; }
     }
 }
--- src/Lykke.Frontend.WampHost/Settings/SessionServiceSettings.cs
+++ src/Lykke.Frontend.WampHost/Settings/SessionServiceSettings.cs
@@ -1,7 +1,10 @@
-﻿namespace Lykke.Frontend.WampHost.Settings
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Frontend.WampHost.Settings
 {
     public class SessionServiceSettings
     {
+        [HttpCheck("/api/isalive")]
         public string SessionServiceUrl { get; set; }
     }
 }
