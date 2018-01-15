96907938244faa3f7b6baf74c9f2775c1643f841
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-01-15 15:41:47 +0300

    changed serializer for trades

--- src/Lykke.Frontend.WampHost.Core/Services/Trades/ITradesManager.cs
+++ src/Lykke.Frontend.WampHost.Core/Services/Trades/ITradesManager.cs
@@ -1,9 +1,10 @@
-﻿using Lykke.Job.TradesConverter.Contract;
+﻿using System.Collections.Generic;
+using Lykke.Job.TradesConverter.Contract;
 
 namespace Lykke.Frontend.WampHost.Core.Services.Trades
 {
     public interface ITradesManager
     {
-        void ProcessTrade(TradeLogItem message);
+        void ProcessTrade(List<TradeLogItem> messages);
     }
 }
