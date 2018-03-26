4645f71c5718c43d9a6c3f9493902d4f3d107b1a
Nikoloz Gamkrelidze <>
2018-03-26 04:35:59 +0200

    added null values handling

--- src/Lykke.Frontend.WampHost.Core/Services/Orders/OutgoingMessages/Order.cs
+++ src/Lykke.Frontend.WampHost.Core/Services/Orders/OutgoingMessages/Order.cs
@@ -1,13 +1,23 @@
-﻿namespace Lykke.Frontend.WampHost.Core.Services.Orders.OutgoingMessages
+﻿using Newtonsoft.Json;
+
+namespace Lykke.Frontend.WampHost.Core.Services.Orders.OutgoingMessages
 {
     public class Order
     {
         public string Id { set; get; }
+        
         public string Status { set; get; }
+        
         public string AssetPairId { set; get; }
+        
+        [JsonProperty(NullValueHandling=NullValueHandling.Include)]
         public double? Price { set; get; }
+        
         public double Volume { set; get; }
+        
+        [JsonProperty(NullValueHandling=NullValueHandling.Include)]
         public double? RemainingVolume { set; get; }
+        
         public bool Straight { set; get; }
     }
 }
