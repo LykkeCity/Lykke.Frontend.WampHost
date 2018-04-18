c75758ad11410324581f27963179ad05d556440c
Sergey Polonsky <polonsky.sergey@hotmail.com>
2018-04-18 19:50:03 +0300

    Removed ClientId from orderbook feed.

--- src/Lykke.Frontend.WampHost.Core/Services/Orderbook/OrderbookMessage.cs
+++ src/Lykke.Frontend.WampHost.Core/Services/Orderbook/OrderbookMessage.cs
@@ -22,7 +22,6 @@ namespace Lykke.Frontend.WampHost.Core.Services.Orderbook
     public class VolumePrice
     {
         public string Id { get; set; }
-        public string ClientId { get; set; }
         public double Volume { get; set; }
         public double Price { get; set; }
     }
