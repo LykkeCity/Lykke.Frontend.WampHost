fadb960af92653da84243b3cd3a1ca9546435bba
Roman Chuprikov <paradise.develop@gmail.com>
2018-09-19 01:54:41 +0500

    LWDEV-9481: handle operation events and commands

---
+++ src/Lykke.Frontend.WampHost.Core/Services/Operations/OperationStatusChangedMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Lykke.Frontend.WampHost.Core.Services.Operations
+{
+    public enum OperationStatus
+    {
+        ConfirmationRequested,
+        Confirmed,
+        Failed,
+    }
+
+    public class OperationStatusChangedMessage
+    {
+        public Guid OperationId { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public OperationStatus Status { get; set; }
+
+        public string ErrorCode { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
+
---
+++ src/Lykke.Frontend.WampHost.Services/CommandHandlers/ConfirmationCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Common.Log;
+using Lykke.Cqrs;
+using Lykke.Frontend.WampHost.Contracts.Commands;
+using Lykke.Frontend.WampHost.Core.Services.Operations;
+using Lykke.Frontend.WampHost.Core.Services.Security;
+using WampSharp.V2;
+using WampSharp.V2.Core.Contracts;
+using WampSharp.V2.Realm;
+
+namespace Lykke.Frontend.WampHost.Services.CommandHandlers
+{
+    public class ConfirmationCommandHandler
+    {
+        private readonly IWampSubject _subject;
+        private readonly ISessionCache _sessionCache;
+
+        private const string Topic = "operations";
+
+        public ConfirmationCommandHandler(
+            IWampHostedRealm realm,
+            ISessionCache sessionCache)
+        {
+            _subject = realm?.Services.GetSubject(Topic);
+            _sessionCache = sessionCache;
+        }
+
+        public async Task<CommandHandlingResult> Handle(RequestConfirmationCommand command, IEventPublisher eventPublisher)
+        {
+            var sessionIds = _sessionCache.GetSessionIds(command.ClientId.ToString());
+            if (sessionIds.Length == 0)
+                return CommandHandlingResult.Ok();
+
+            _subject.OnNext(new WampEvent
+            {
+                Options = new PublishOptions { Eligible = sessionIds },
+                Arguments = new object[]
+                {
+                    new OperationStatusChangedMessage
+                    {
+                        Status = OperationStatus.ConfirmationRequested,
+                        OperationId = command.OperationId
+                    }
+                }
+            });
+
+            return CommandHandlingResult.Ok();
+        }
+    }
+}
