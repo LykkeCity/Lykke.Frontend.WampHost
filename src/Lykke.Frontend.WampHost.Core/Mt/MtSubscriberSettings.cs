2103b4a995a060192f81e56db1224a51fa78b883
Sergey Polonsky <polonsky.sergey@hotmail.com>
2019-02-02 15:55:51 +0300

    Disabled MT support. All related code is deleted.

--- src/Lykke.Frontend.WampHost.Core/Mt/MtSubscriberSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lykke.Frontend.WampHost.Core.Mt
-{
-    public class MtSubscriberSettings
-    {
-        public string LiveRabbitMqConnString { get; set; }
-        public string DemoRabbitMqConnString { get; set; }
-        public MtExchangesNamesSettings Exchanges { get; set; }
-    }
-}
